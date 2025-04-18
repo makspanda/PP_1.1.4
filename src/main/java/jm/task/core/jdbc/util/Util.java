095e335078943771660db7865ceb1e0ac27cdcb1
makspanda <makspanda3010@gmail.com>
2025-04-19 00:33:20 +0700

    Исправление

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -4,14 +4,28 @@ package jm.task.core.jdbc.util;
 import jm.task.core.jdbc.model.User;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
+import java.util.Properties;
 
 public class Util {
     private static final SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory() {
         try {
-            return new Configuration()
-                    .configure()
+            Configuration configuration = new Configuration();
+
+            Properties settings = new Properties();
+
+            settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+            settings.put(Environment.URL, "jdbc:mysql://localhost:3306/dbtest");
+            settings.put(Environment.USER, "Admin");
+            settings.put(Environment.PASS, "Tru124~k78!");
+            settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+            settings.put(Environment.HBM2DDL_AUTO, "update");
+            settings.put(Environment.SHOW_SQL, "true");
+
+            return configuration.setProperties(settings)
                     .addAnnotatedClass(User.class)
                     .buildSessionFactory();
         } catch (Throwable ex) {
@@ -22,7 +36,5 @@ public class Util {
 
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
-        }
-
-
+    }
 }
